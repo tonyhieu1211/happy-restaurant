e1ff48021527258337d334aa001934d5bf762131
tran minh hieu <tranminhhieu9901@gmail.com>
2021-02-09 15:07:18 +0700

    fetch data from cms and render to home page

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,28 @@
 const express = require('express');
-
+const controllers = require('../controllers');
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.render('home', req.context);
+router.get('/',async (req, res, next)=>{
+    const data = req.context;
+    const itemCtr = controllers.item.instance();
+    data.breakfast = await itemCtr.get({category: 'Breakfast'});
+    data.lunch = await itemCtr.get({category: 'Lunch'});
+    data.dinner = await itemCtr.get({category: 'Dinner'});
+    data.desserts = await itemCtr.get({category: 'Dessert'});
+    data.wine = await itemCtr.get({category: 'Wine'});
+    data.drinks = await itemCtr.get({category: 'Drink'});
+
+    res.render('home', data);
 });
 
 router.get('/blog', (req, res, next)=>{
     res.render('blog', req.context);
 });
 
+router.get('/items',async (req, res, next)=>{
+    const itemCtr = controllers.item.instance();
+    const items = await itemCtr.get();
+    res.json({items});
+});
 
 module.exports = router;
